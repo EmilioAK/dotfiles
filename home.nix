5e9d7be0acd6a2f3244fdd3533bdfe746c99f2a4
EmilioAK <me@emilioak.dev>
2026-04-27 14:51:06 +0200

    Add better dotfile support

--- home.nix
+++ home.nix
@@ -1,15 +1,16 @@
-{ pkgs, username, ... }: {
+{ pkgs, config, username, ... }:
+let
+  flakeRoot = "${config.home.homeDirectory}/.config/nix-darwin";
+  dotfile = path: config.lib.file.mkOutOfStoreSymlink "${flakeRoot}/dotfiles/${path}";
+in {
   home.username = username;
   home.homeDirectory = "/Users/${username}";
   home.stateVersion = "25.11";
 
   programs.home-manager.enable = true;
 
-  home.file.".gitconfig".text = ''
-    [user]
-      name = EmilioAK
-      email = me@emilioak.dev
-  '';
+  home.file.".gitconfig".source = dotfile "gitconfig";
+  xdg.configFile."ghostty/config".source = dotfile "ghostty/config";
 
   programs.zsh = {
     enable = true;

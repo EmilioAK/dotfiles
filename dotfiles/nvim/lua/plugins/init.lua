3f6873634341f1305f05cd9887f24b49eae849f6
EmilioAK <me@emilioak.dev>
2026-04-28 23:38:00 +0200

    Add support for zsh (for the remote vps)

--- dotfiles/nvim/lua/plugins/init.lua
+++ dotfiles/nvim/lua/plugins/init.lua
@@ -1,9 +1,17 @@
+local function terminal_shell()
+  if (vim.uv or vim.loop).os_uname().sysname == "Darwin" then
+    return { "/opt/homebrew/bin/fish", "--login" }
+  end
+
+  return { "zsh", "--login" }
+end
+
 return {
   {
     "folke/snacks.nvim",
     opts = {
       terminal = {
-        shell = { "/opt/homebrew/bin/fish", "--login" },
+        shell = terminal_shell(),
       },
     },
   },
